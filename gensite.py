5e7c147474ec876252eea4d252139c03f40002a4
Ian Pye <ianpye@gmail.com>
2026-03-27 13:36:44 -0700

    Adding a new photo section

--- gensite.py
+++ gensite.py
@@ -4,10 +4,11 @@ from liquid import parse
 from liquid import render
 
 from os import listdir
-from os.path import isfile, join
+from os.path import isfile, join, isdir
 
 import json
 
+# Given a template directory, outout rendered templates to output_dir.
 def generate(templates_dir: string, output_dir: string, data_file: string) -> int:
     env = Environment(loader=CachingFileSystemLoader(templates_dir, ext=".html"))
 
@@ -25,17 +26,10 @@ def generate(templates_dir: string, output_dir: string, data_file: string) -> in
         if f in data:
             data[f]["url"] = f
             data[f]["name"] = f.replace(".html", "")
-
             if "img_dir" in data[f]:
-                imgs = []
-                try:
-                    for img in listdir(join(output_dir, data[f]["img_dir"])):
-                        fullimg = join(output_dir, data[f]["img_dir"], img)
-                        if isfile(fullimg):
-                            imgs.append(join(data[f]["img_dir"], img))
-                    data[f]["imgs"] = imgs
-                except FileNotFoundError:
-                    print("Skipping image dir %s" % (join(output_dir, data[f]["img_dir"])))
+                cat = {}
+                _ = loadimgs(data[f]["img_dir"], output_dir, cat)
+                data[f]["imgs"] = cat
 
             with open("%s/%s" % (output_dir, f), "w") as file:
                 file.write(template.render(page = data[f]))
@@ -44,3 +38,19 @@ def generate(templates_dir: string, output_dir: string, data_file: string) -> in
             print("Skipping %s. Add this file to %s if needed." % (f, data_file))
 
     return written
+
+# Given an images directory, find categorized images for a given template.
+def loadimgs(img_dir: string, output_dir: string, imgcat: dict) -> []:
+    imgs = []
+    try:
+        for img in listdir(join(output_dir, img_dir)):
+            fullimg = join(output_dir, img_dir, img)
+            if isfile(fullimg):
+                imgs.append(join(img_dir, img))
+            if isdir(fullimg):
+                imgcat[img] = loadimgs(join(img_dir, img), output_dir, imgcat)
+
+    except FileNotFoundError:
+        print("Skipping missing image dir %s" % (join(output_dir, img_dir)))
+
+    return imgs
